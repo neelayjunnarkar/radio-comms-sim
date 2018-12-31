ab1fcfcad3c141f6537d529237bac68b1900a6a6
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2018-12-30 17:16:35 -0800

    fixed only some messages sending

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,11 @@ struct Packet {
     payload: String,
     checksum: u8,
 }
+
+const PREAMBLE_LEN: usize = 256;
 const PACKET_LEN_IDX: usize = 3;
 const PACKET_MAX_LEN: usize = 63; // floor(255/4) = 63 where max utf-8 char len is 4 bytes and 255 is max number representable by u8
+
 struct Radio {
     tx: mpsc::Sender<Vec<u8>>,
     frame_id: u8,
@@ -67,6 +70,7 @@ impl Radio {
                     encoded_p_res.map(|mut encoded_p| {
                         let len = encoded_p.len();
                         encoded_p[len - 1] = encoded_p.iter().fold(0, |acc, x| acc ^ x);
+                        // println!("checksum: {}", encoded_p[len -1]);
                         encoded_p
                     })
                 });
@@ -89,8 +93,8 @@ fn main() {
     let (tx_medium, rx_radio2) = mpsc::channel::<u8>();
     let mut radio = Radio::new(tx_radio1);
 
-    let preamble: [u8; 256] = {
-        let mut preamble: [u8; 256] = [0; 256];
+    let preamble: [u8; PREAMBLE_LEN] = {
+        let mut preamble: [u8; PREAMBLE_LEN] = [0; PREAMBLE_LEN];
         let d = Uniform::new_inclusive(0u8, 255);
         for i in 0..preamble.len() {
             preamble[i] = d.sample(&mut rand::thread_rng());
@@ -110,7 +114,6 @@ fn main() {
         let mut counter = 0;
         let mut buf: VecDeque<Vec<u8>> = VecDeque::new();
         let mut curr_packet: Option<Vec<u8>> = None;
-
         loop {
             // push new received packets to buf
             while let Ok(packet) = rx_medium.try_recv() {
@@ -162,7 +165,6 @@ fn main() {
         let mut buf: [u8; 2048] = [0; 2048];
         let mut curr_idx = 0;
         let mut rx_state = RxState::PreambleCheck;
-
         for received in rx_radio2 {
             match rx_state {
                 RxState::PreambleCheck => {
@@ -180,7 +182,7 @@ fn main() {
                     buf[curr_idx] = received;
                     curr_idx += 1;
                     if curr_idx > PACKET_LEN_IDX
-                        && curr_idx >= (buf[PACKET_LEN_IDX] as usize) + 5 + 1
+                        && curr_idx >= (buf[PACKET_LEN_IDX] as usize) + 5
                     {
                         if buf.iter().take(curr_idx).fold(0, |acc, x| acc ^ x) == 0 {
                             if let Ok(decoded_packet) = deserialize::<Packet>(&buf[0..curr_idx]) {
@@ -192,24 +194,22 @@ fn main() {
                             println!("checksum failed");
                         }
                         rx_state = RxState::PreambleCheck;
+                        curr_idx = 0;
                     }
                 }
             }
-            // if received.iter().fold(0, |acc, x| acc ^ x) == 0 {
-            //     if let Ok(decoded_p) = deserialize::<Packet>(&received) {
-            //         // println!("{:?}", received);
-            //         println!("{:?}", decoded_p);
-            //     } else {
-            //         println!("failed to deserialize");
-            //     }
-            // } else {
-            //     println!("Error in received message");
-            //     println!("{:?}", received);
-            // }
         }
     });
 
-    radio.transmit("abcdefghijklmnopqrstuvwxyz1234567890 abcdefghijklmnopqrstuvwxyz1234567890 abcdefghijklmnopqrstuvwxyz1234567890 abcdefghijklmnopqrstuvwxyz1234567890 abcdefghijklmnopqrstuvwxyz1234567890".to_string()).expect("failed tx");
+    radio.transmit("transmissions begin now".to_string()).expect("failed tx");
+
+    radio.transmit("abcdefghijklmnopqrstuvwxyz1234567890 abcdefghijklmnopqrstuvwxyz1234567890
+    abcdefghijklmnopqrstuvwxyz1234567890 ".to_string()).expect("failed tx");
+    
+    radio.transmit("transmissions end now".to_string()).expect("failed tx");
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
     rx_thread.join().expect("failed to join rx thread");
     medium_thread.join().expect("failed to join medium thread");
 }
+ 
