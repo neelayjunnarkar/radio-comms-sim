648b41719ff788d94a076e652331204563dc7056
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-05-08 20:12:29 -0700

    plays notes when transmititng

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ const CHANNELS: i32 = 2;
 const SAMPLE_RATE: f64 = 10000.0;
 const FRAMES_PER_BUFFER: u32 = 64;
 const TABLE_SIZE: usize = 10000;
+const SEND_INTERVAL_MS: u64 = 10;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Packet {
@@ -83,12 +84,14 @@ impl Radio {
                     })
                 })
                 .map(|encoded_p_res| {
-                    encoded_p_res.map(|encoded_p| encoded_p.iter().map(|&byte| unpack_byte(byte))
-                                      .collect::<Vec<[u8;8]>>())
+                    encoded_p_res.map(|encoded_p| {
+                        encoded_p
+                            .iter()
+                            .map(|&byte| unpack_byte(byte))
+                            .collect::<Vec<[u8; 8]>>()
+                    })
                 })
-                .map(|encoded_p_res| {
-                    encoded_p_res.map(|encoded_p| flatten(encoded_p))
-                });
+                .map(|encoded_p_res| encoded_p_res.map(|encoded_p| flatten(encoded_p)));
             for encoded_packet_res in encoded_packets {
                 if let Ok(encoded_packet) = encoded_packet_res {
                     if let Err(_) = self.tx.send(encoded_packet) {
@@ -110,7 +113,7 @@ fn main() {
 
     let preamble: [u8; PREAMBLE_LEN] = {
         let mut preamble: [u8; PREAMBLE_LEN] = [0; PREAMBLE_LEN];
-        let d = Uniform::new_inclusive(0u8, 255);
+        let d = Uniform::new_inclusive(0u8, 1);
         for i in 0..preamble.len() {
             preamble[i] = d.sample(&mut rand::thread_rng());
         }
@@ -118,18 +121,66 @@ fn main() {
     };
 
     let medium_thread = std::thread::spawn(move || {
+        let note_0 = 440.0;
+        let note_1 = 560.0;
+        
+        let mut sine_0 = [0.0; TABLE_SIZE];
+        let mut sine_1 = [0.0; TABLE_SIZE];
+        for i in 0..TABLE_SIZE {
+            sine_0[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0 * note_0 as f64).sin() as f32;
+            sine_1[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0 * note_1 as f64).sin() as f32;
+        }
+        let sine_0 = sine_0;
+        let sine_1 = sine_1;
+
+        let mut phase = 0;
+        let pa = pa::PortAudio::new().expect("faillll on port audio");
+        let settings = pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)
+            .expect("fail on settings");
+
+        let sines = [sine_0, sine_1];
+        let mut sines_idx = 0;
+
+        let (tx_note, rx_note) = mpsc::channel();
+
+        let mut last_note_idx = 0;
+        let cb = move |pa::OutputStreamCallbackArgs {buffer, frames, ..}| {
+            
+            last_note_idx = if let Ok(sines_idx) = rx_note.try_recv() {
+                sines_idx
+            } else {
+                last_note_idx
+            };
+
+            let mut idx = 0;
+            for _ in 0..frames {
+                buffer[idx] = sines[last_note_idx][phase];
+                buffer[idx+1] = sines[last_note_idx][phase];
+                phase += 1;
+                if phase >= TABLE_SIZE {
+                    phase -= TABLE_SIZE;
+                }
+                idx += 2;
+            }
+            pa::Continue
+        };
+
+        let mut stream = pa.open_non_blocking_stream(settings, cb).expect("faill on stream");
+        stream.start();
+
         #[derive(PartialEq)]
         enum TxState {
             Random,
             Preamble,
             Packet,
         }
-        let distr = Uniform::new_inclusive(0u8, 255);
+        let distr = Uniform::new_inclusive(0u8, 1);
         let mut tx_state = TxState::Random;
         let mut counter = 0;
         let mut buf: VecDeque<Vec<u8>> = VecDeque::new();
         let mut curr_packet: Option<Vec<u8>> = None;
         loop {
+            tx_note.send(sines_idx);
             while let Ok(packet) = rx_medium.try_recv() {
                 buf.push_back(packet);
             }
@@ -143,11 +194,14 @@ fn main() {
             }
             match tx_state {
                 TxState::Random => {
+                    let num = distr.sample(&mut rand::thread_rng());
+                    sines_idx = num as usize;
                     tx_medium
-                        .send(distr.sample(&mut rand::thread_rng()))
+                        .send(num)
                         .unwrap_or(());
                 }
                 TxState::Preamble => {
+                    sines_idx = preamble[counter] as usize;
                     while tx_medium.send(preamble[counter]).is_err() {}
                     counter += 1;
                     if counter >= preamble.len() {
@@ -157,6 +211,7 @@ fn main() {
                 }
                 TxState::Packet => {
                     if let Some(ref packet) = curr_packet {
+                        sines_idx = (*packet)[counter] as usize;
                         while tx_medium.send((*packet)[counter]).is_err() {}
                         counter += 1;
                         if counter >= packet.len() {
@@ -168,7 +223,7 @@ fn main() {
                     }
                 }
             }
-            std::thread::sleep(std::time::Duration::from_millis(1));
+            std::thread::sleep(std::time::Duration::from_millis(SEND_INTERVAL_MS));
         }
     });
 
@@ -204,9 +259,12 @@ fn main() {
                         curr_buf_idx += 1;
                         curr_byte_idx = 0;
                     }
-                    if curr_buf_idx > PACKET_LEN_IDX && curr_buf_idx >= (buf[PACKET_LEN_IDX] as usize) + 5 {
+                    if curr_buf_idx > PACKET_LEN_IDX
+                        && curr_buf_idx >= (buf[PACKET_LEN_IDX] as usize) + 5
+                    {
                         if buf.iter().take(curr_buf_idx).fold(0, |acc, x| acc ^ x) == 0 {
-                            if let Ok(decoded_packet) = deserialize::<Packet>(&buf[0..curr_buf_idx]) {
+                            if let Ok(decoded_packet) = deserialize::<Packet>(&buf[0..curr_buf_idx])
+                            {
                                 println!("{:?}", decoded_packet);
                             } else {
                                 println!("failed to deserialize");
@@ -245,31 +303,31 @@ fn main() {
 }
 
 fn unpack_byte(input: u8) -> [u8; 8] {
-    let mut out = [0, 0, 0, 0, 0, 0, 0, 0];
-    out[0] = (input >> 0) & 1;
-    out[1] = (input >> 1) & 1;
-    out[2] = (input >> 2) & 1;
-    out[3] = (input >> 3) & 1;
-    out[4] = (input >> 4) & 1;
-    out[5] = (input >> 5) & 1;
-    out[6] = (input >> 6) & 1;
-    out[7] = (input >> 7) & 1;
-    out
+    [
+        (input >> 0) & 1,
+        (input >> 1) & 1,
+        (input >> 2) & 1,
+        (input >> 3) & 1,
+        (input >> 4) & 1,
+        (input >> 5) & 1,
+        (input >> 6) & 1,
+        (input >> 7) & 1,
+    ]
 }
 
 fn pack_byte(arr: [u8; 8]) -> u8 {
-   (arr[0] << 0)
-       | (arr[1] << 1)
-       | (arr[2] << 2)
-       | (arr[3] << 3)
-       | (arr[4] << 4)
-       | (arr[5] << 5)
-       | (arr[6] << 6)
-       | (arr[7] << 7)
+    (arr[0] << 0)
+        | (arr[1] << 1)
+        | (arr[2] << 2)
+        | (arr[3] << 3)
+        | (arr[4] << 4)
+        | (arr[5] << 5)
+        | (arr[6] << 6)
+        | (arr[7] << 7)
 }
 
 fn flatten(vec: Vec<[u8; 8]>) -> Vec<u8> {
-    let mut out = Vec::with_capacity(vec.len()*8);
+    let mut out = Vec::with_capacity(vec.len() * 8);
     for arr in vec.iter() {
         for &byte in arr.iter() {
             out.push(byte);
@@ -277,4 +335,3 @@ fn flatten(vec: Vec<[u8; 8]>) -> Vec<u8> {
     }
     out
 }
-
